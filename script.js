a2b086553c2d866e4e25e64889d494fc7da4a628
Manir96 <maniruzzaman.manir96@gmail.com>
2024-11-12 00:03:46 +0600

    Responsive Done

--- script.js
+++ script.js
@@ -17,3 +17,27 @@ scrollUpButton.addEventListener('click', () => {
 		behavior: 'smooth' // Smooth scrolling effect
 	});
 });
+
+
+$(document).ready(function(){
+	$('.mobile_menu_icon').click(function(){
+		let mobileMenu = $('.nav-links');
+		if(mobileMenu.hasClass('display_none')){
+			mobileMenu.removeClass('display_none');
+		}
+		else{
+			mobileMenu.addClass('display_none');
+		}
+
+	})
+})
+
+// let mobileIcon = document.querySelector('.mobile_menu_icon');
+// let mobileMenu = document.querySelector('.nav-links');
+// mobileIcon.addEventListener('click', function(){
+// 	mobileMenu.style.display = 'none';
+// 	if(mobileMenu.hasClass){
+// 		.display_none
+// 	}
+	
+// })
